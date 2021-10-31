039fcabcad64630f8f40336706ca273620dbaa4c
João Filipe Matos Silva <joao@kspace.pt>
2021-10-31 16:28:04 +0000

    Add Member

---
+++ TarsOffice/Pages/Teams/AddMember.cshtml.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TarsOffice.Data;
+using TarsOffice.Extensions;
+using TarsOffice.Viewmodel;
+
+namespace TarsOffice.Pages.Teams
+{
+    public class AddMember : PageModel
+    {
+        private readonly TarsOffice.Data.ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public AddMember(TarsOffice.Data.ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            this.userManager = userManager;
+        }
+
+        public async Task<IActionResult> OnGetAsync(Guid id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Team = await _context.Teams
+                .Include(x => x.Members)
+                .ThenInclude(y => y.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Team == null)
+            {
+                return NotFound();
+            }
+
+            var userId = User.GetId();
+            if (!Team.Members.Any(member => member.UserId == userId))
+            {
+                return Forbid();
+            }
+
+            return Page();
+        }
+
+        public Team Team { get; set; }
+
+        [BindProperty]
+        public NewTeamMember TeamMember { get; set; }
+
+        public async Task<IActionResult> OnPostAsync(Guid id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Team = await _context.Teams
+                .Include(x => x.Members)
+                .ThenInclude(y => y.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Team == null)
+            {
+                return NotFound();
+            }
+
+            var userId = User.GetId();
+            if (!Team.Members.Any(member => member.UserId == userId))
+            {
+                return Forbid();
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _context.Users.FirstOrDefaultAsync(x => x.UserName == TeamMember.UserName);
+            if(user == null)
+            {
+                user = new IdentityUser(TeamMember.UserName);
+                var result = await userManager.CreateAsync(user);
+                if(!result.Succeeded)
+                {
+                    return Page();
+                }
+            }
+
+            var newTeamMember = new TeamMember
+            {
+                User = user,
+                IsAdmin = TeamMember.IsAdmin,
+                Team = Team
+            };
+
+            _context.TeamMembers.Add(newTeamMember);
+            await _context.SaveChangesAsync();
+            return RedirectToPage("Details", new { Id = id });
+        }
+    }
+}
---
+++ TarsOffice/Viewmodel/NewTeamMember.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TarsOffice.Viewmodel
+{
+    public class NewTeamMember
+    {
+        [Required]
+        public string UserName { get; set; }
+
+        public bool IsAdmin { get; set; }
+    }
+}
