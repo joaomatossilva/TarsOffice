b786a1998a78d403f1376dc837f134c48c84e432
João Filipe Matos Silva <joao@kspace.pt>
2021-11-01 00:28:36 +0000

    Run migrations on startup

--- TarsOffice/Program.cs
+++ TarsOffice/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TarsOffice.Data;
 
 namespace TarsOffice
 {
@@ -13,7 +16,16 @@ namespace TarsOffice
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            //Apply migrations on Startup. This is very un-recomended
+            using (var scope = host.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                db.Database.Migrate();
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
