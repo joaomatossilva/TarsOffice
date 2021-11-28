b5f358052d4a59e436017048f0e761062b022f9b
João Filipe Matos Silva <joao@kspace.pt>
2021-11-28 22:39:14 +0000

    Better day formatting

--- TarsOffice/Startup.cs
+++ TarsOffice/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -85,6 +86,11 @@ namespace TarsOffice
 
             services.AddScoped<ISiteService, SiteService>();
             services.AddHttpContextAccessor();
+
+            //just some hack stuff to avoid globalization right now
+            var cultureInfo = new CultureInfo("en-US");
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
